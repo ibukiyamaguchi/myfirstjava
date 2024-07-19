a9dbedb8e267a0e2da91bfbd46d92e6dc816c923
Ibuki Yamaguchi <ibukiyamaguchi@gmail.com>
2024-07-20 01:07:49 +0900

    add controller say hello

---
+++ src/main/java/com/example/hello/HelloApplicationController.java
@@ -0,0 +1,12 @@
+package com.example.hello;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloApplicationController {
+
+    @RequestMapping("/")  // ルートへこのメソッドをマップする
+    public String test() {
+        return "Hello World";
+    }
+}
